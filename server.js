0c4b69ab89c04a8515a84bf88009013de7c2c3af
honeypotfly <honeypotfly@protonmail.com>
2022-03-20 00:13:27 -0400

    clean typos

--- server.js
+++ server.js
@@ -1,46 +1,58 @@
-const { Client } = require("pg")
-const express = require("express")
-const app = express()
-const port = 8080
+// Import the postgres client
+const { Client } = require("pg");
+const express = require("express");
+const app = express();
+const port = 8080;
 
+// Connect to our postgres database
+// These values like `root` and `postgres` will be
+// defined in our `docker-compose-yml` file
 const client = new Client({
-    user: "root",
-    password: "root",
-    host: "postgres",
+  password: "root",
+  user: "root",
+  host: "postgres",
 });
 
+
+// Serves a folder called `public` that we will create
 app.use(express.static("public"));
 
-
-app.get("/employees", async(req, res) => {
-    const results = await client
-        .query("SELECT * FROM employees")
-        .then((payload) => {
-            return payload.rows;
-        })
-        .catch(() => {
-            throw new Error("Query failed");
-        });
-    res.setHeader("Content-Type", "application/json");
-    res.status(200);
-    res.send(JSON.stringify(results))
+// When a GET request is made to /employees
+// Our app will return an array with a list of all
+// employees including name and title
+// this data is defined in our `database-seed.sql` file
+app.get("/employees", async (req, res) => {
+  const results = await client
+    .query("SELECT * FROM employees")
+    .then((payload) => {
+      return payload.rows;
+    })
+    .catch(() => {
+      throw new Error("Query failed");
+    });
+  res.setHeader("Content-Type", "application/json");
+  res.status(200);
+  res.send(JSON.stringify(results));
 });
 
+// Our app must connect to the database before it starts, so
+// we wrap this in an IIFE (Google it) so that we can wait
+// asynchronously for the database connection to establish before listening
 (async () => {
-    await client.connect();
+  await client.connect();
 
-    app.listen(port, () => {
-        console.log(`Example app listening at http://localhost:${port}`);
-    });
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
 })();
 
 const myPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve("foo");
-    }, 300);
-    reject("oops");
+  setTimeout(() => {
+    resolve("foo");
+  }, 300);
+  reject("oops");
 });
 
 myPromise.then(() => {
-    console.log("hello")
+  console.log("hello");
 });
